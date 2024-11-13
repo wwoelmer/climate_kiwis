bfc3edb80ba56405a028a7f1e34bd8b0805aa7f0
wwoelmer <wwoelmer@vt.edu>
2024-11-14 11:53:36 +1300

    clean up figures, order factor, calculate mean trend by region

--- scripts/01_watertemp_change_by_30_districts.R
+++ scripts/01_watertemp_change_by_30_districts.R
@@ -48,6 +48,7 @@ data <- data %>%
        n = n(),
        time_since_previous_obs = as.numeric(difftime(as.Date(Date), lag(as.Date(Date)), units = "days")),
        lake_max_gap = ifelse(n > 1, max(time_since_previous_obs, na.rm = TRUE), NA)) 
+
 data <- data %>% 
   group_by(LID) %>% 
   mutate(n_lakes = n())
@@ -96,6 +97,7 @@ compare <- left_join(out, sen, by = c('LID', 'district'))
 
 ggplot(compare, aes(x = V1, y = sen_slope, color = district)) +
   geom_point() +
+  geom_abline(slope = 1, intercept = 0, size = 2, color = 'black', linetype = 'dashed') +
   geom_smooth(method = 'lm') +
   theme(legend.position = 'none')
 
@@ -103,14 +105,99 @@ ggplot(out, aes(x = V1, y = district, fill = district)) +
   geom_density_ridges() +
   xlab('Rate of change') +
   geom_vline(xintercept = 0) +
-  theme(legend.position = 'none')
+  theme_bw() +
+  theme(legend.position = 'none') 
 
-ggplot(sen, aes(x = sen_slope, y = district, fill = district)) +
+##################
+# ADD REGION AS COLUMN, SPLIT DISTRICT
+sen <- sen %>% 
+  mutate(region = sub(".*\\((.*)\\)", "\\1", district),
+         city = sub(" \\(.*\\)", "", district)) %>% 
+  mutate(island = ifelse(region %in% c('Northland',
+                                       'Auckland', 'Waikato',
+                                       'Bay of Plenty',
+                                       'Gisborne', "Hawke's Bay", 
+                                       'Manawatu-Whanganui', 'Taranaki', 
+                                       'Wellington'), 
+                         'North', 'South')) %>% 
+  arrange(region, city)
+
+
+sen$region <- factor(sen$region, levels = c('Northland',
+                                            'Auckland', 'Waikato',
+                                            'Bay of Plenty',
+                                            'Gisborne', "Hawke's Bay", 
+                                            'Manawatu-Whanganui', 'Taranaki', 
+                                            'Wellington',
+                                            'Nelson', "Marlborough",
+                                            'West Coast','Canterbury', 
+                                            'Otago',
+                                            'Southland'))
+
+sen$city <- factor(sen$city, levels = c('Kerikeri', 'Whangarei',
+                                        'Whangaparaoa', 'Auckland',
+                                        'Hamilton', 'Taupo',
+                                        'Rotorua', 'Tauranga',
+                                        'Gisborne', 'Napier', 
+                                        'Dannevirke', 'Taumarunui',
+                                        'Waiouru', 'Whanganui',
+                                        'New Plymouth', 'Masterton',
+                                        'Wellington', 'Nelson',
+                                        'Blenheim', 'Hokitika',
+                                        'Reefton', 'Christchurch',
+                                        'Lake Tekapo', 'Tara Hills',
+                                        'Timaru', 'Dunedin',
+                                        'Queenstown', 'Gore',
+                                        'Invercargill', 'Milford Sound'))
+
+  
+a <- ggplot(sen, aes(x = sen_slope)) +
+  geom_density(size = 2) +
+  xlab('Rate of change in LSWT (°C/decade)') +
+  ylab('Density') +
+  geom_vline(xintercept = 0) +
+  theme_bw() 
+
+ggplot(sen, aes(x = sen_slope, y = city, fill = region)) +
   geom_density_ridges() +
   xlab('Rate of change') +
   geom_vline(xintercept = 0) +
-  theme(legend.position = 'none')
+  theme_bw() 
 
+# set up color palette
+n_colors <- length(levels(sen$region))
+colors <- colorRampPalette(brewer.pal(11, "Spectral"))(n_colors)
+
+sen <- sen %>% 
+  arrange(region, city) 
+
+b <- ggplot(sen, aes(x = sen_slope, y = fct_rev(city), fill = region)) +
+  geom_density_ridges(scale = 2) +
+  xlab('Rate of change in LSWT (°C/decade)') +
+  coord_cartesian(clip = 'off') +      # Allow plot to extend beyond the default area
+  ylab('City') +
+  scale_y_discrete(expand = expansion(mult = c(0.05, 0.1))) +  # Add space between ridges
+  scale_fill_manual(values = colors) +
+#  facet_wrap(~island, scales = 'free_y') +
+  geom_vline(xintercept = 0) +
+  labs(fill = 'Region') +
+  theme_bw()
+b
+lswt_rate <- ggarrange(a, b, widths = c(1, 2), 
+                       labels = 'auto')
+ggsave('./figures/rate_of_change_LWST.png', lswt_rate, 
+       dpi = 300, units = 'mm', height = 400, width = 800, scale = 0.3)
+
+# mean trends by region
+reg_trend <- sen %>% 
+  group_by(region) %>% 
+  summarise(mean_sen = mean(sen_slope, na.rm = TRUE),
+            sd = sd(sen_slope, na.rm = TRUE))
+
+reg_trend <- reg_trend %>% 
+  mutate(mean_sen = round(mean_sen, 2),
+         sd = round(sd, 2))
+################################################################################
 out <- out %>% 
   group_by(district) %>% 
   mutate(n_lakes= n())
