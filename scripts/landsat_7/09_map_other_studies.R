3f886475c84e36ce759b03ae47ec83c084de4d74
wwoelmer <wwoelmer@vt.edu>
2025-08-13 16:05:41 +1200

    lots of work on figure 1 map and histos

--- scripts/landsat_7/09_map_other_studies.R
+++ scripts/landsat_7/09_map_other_studies.R
@@ -11,8 +11,10 @@ library(MetBrewer)
 library(viridis)
 library(ggpubr)
 
-gps <- read.csv('./data/LSWT_rates_of_change_literature_16June2025.csv',
+gps <- read.csv('./data/LSWT_rates_of_change_literature.csv',
                 fileEncoding = 'latin1')
+
+gps$rate_C_year <- as.numeric(gps$rate_C_year)
 
 # get rid of this study on the map
 gps <- gps %>% 
@@ -30,12 +32,31 @@ gps$spatial_extent <- factor(gps$spatial_extent, levels = c('point', 'region', '
 gps <- gps %>% 
   filter(!is.na(rate_C_year))
 
+table(gps$spatial_extent)
+table(gps$location)
+table(gps$method_LSWT)
+table(gps$method_LSWT_standard)
+table(gps$method_trend)
+table(gps$aggregation_mean_min_max_etc)
+table(gps$temporal_aggregation)
+max(gps$n_lakes)
+median(gps$n_lakes)
+median(gps$n_years)
+min(gps$n_years)
+max(gps$n_years)
+min(gps$rate_C_year)
+max(gps$rate_C_year)
+
+## night time estimates
+night <- gps %>% 
+  filter(str_detect(annual_seasonal_data, "night")) %>% 
+  distinct(citation)
+nrow(night)
+
 # filter to lakes that we can directly compare
 sub <- gps %>% 
   filter(annual_seasonal_data=='annual',
          aggregation_mean_min_max_etc=='mean')
-
-
 
 # get world shapefile
 world <- ne_countries(scale = "medium", returnclass = "sf")
@@ -156,24 +177,111 @@ mapc <- ggplot() +
     mid = "#D3D3D3",      # neutral
     high = "firebrick",       # for warming
     midpoint = 0) +
-  ggtitle(paste0('All temporal aggregations (annual, summer, etc.) n = ', length(unique(gps$citation))))
+  labs(fill = 'Rate (°C/year)',
+       size = 'Saptial Extent') +
+  guides(
+    fill = guide_colorbar(direction = "vertical"),
+    size = guide_legend(direction = "vertical")
+  ) +
+  theme(
+    legend.position = "right",       # Keep legends on the right
+    legend.box = "horizontal"        # Arrange separate legends side by side
+  ) 
 mapc
 
-table(gps$spatial_extent)
-table(gps$location)
-table(gps$method_LSWT)
-table(gps$method_LSWT_standard)
-table(gps$method_trend)
-table(gps$aggregation_mean_min_max_etc)
-table(gps$temporal_aggregation)
+gps_notglobal <- gps %>% 
+  filter(spatial_extent!='global')
+global <- gps %>% 
+  filter(spatial_extent=='global',
+         method_LSWT!='MOD11L1', # this study has two entries, remove the one using linear regression but keep sen's slope
+         !(citation_short == "Wang et al. 2024" & temporal_aggregation == "summer")) # remove the summer value for the same study
 
-hist(gps$n_years)
-hist(gps$n_lakes)
+p_notglobal <- ggplot() +
+  geom_sf(data = world, fill = 'gray', color = 'gray') +
+  theme_bw() +
+  geom_jitter(data = gps_notglobal, shape = 21, color = 'black',
+              aes(x = long, y = lat, fill = rate_C_year, size = n_lakes))  +
+  scale_fill_gradient2(
+    low = "steelblue",       # for cooling
+    mid = "#D3D3D3",      # neutral
+    high = "firebrick",       # for warming
+    midpoint = 0) +
+  scale_size_continuous(
+    breaks = c(10, 100, 500, 1000),
+    range  = c(1, 10)) + # optional: controls min/max point size in the plot
+  labs(fill = 'Rate (°C/year)',
+       size = 'Number of lakes') +
+  guides(fill = guide_colorbar(direction = "vertical"),
+         size = guide_legend(direction = "vertical")) +
+  theme(legend.position = "right",       # Keep legends on the right
+        legend.box = "horizontal",
+        legend.box.just = 'center')        # Arrange separate legends side by side
+p_notglobal  
 
-gps %>% 
-  filter(n_lakes < 2000) %>% 
-  ggplot(aes(x = n_lakes)) +
-  geom_histogram()
+p_notglobal <- p_notglobal + 
+  geom_point(data = global[global$n_lakes < 2000,], 
+             aes(x = dummy_long, y = dummy_lat,
+             color = rate_C_year, size = n_lakes, alpha = 0.3)) +
+  guides(alpha = 'none',
+         color = 'none') +
+  scale_color_gradient2(
+    low = "steelblue",       # for cooling
+    mid = "#D3D3D3",      # neutral
+    high = "firebrick",       # for warming
+    midpoint = 0) 
+p_notglobal  
+
+p_global <- ggplot() +
+  geom_point(data = global[global$n_lakes < 2000,], 
+             aes(x = dummy_long, y = dummy_lat,
+                 fill = rate_C_year, size = n_lakes)) +
+  scale_fill_gradient2(
+    low = "steelblue",       # for cooling
+    mid = "#D3D3D3",      # neutral
+    high = "firebrick",       # for warming
+    midpoint = 0) +
+  theme_bw() +
+  ylab( 'Rate (°C/year)') +
+  xlab('Study citation') +
+  ggtitle('Global studies') +
+#  guides(size = 'none') +
+  theme(legend.position = "right",       # Keep legends on the right
+        legend.box = "horizontal",
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        text = element_text(size = 12))        
+p_global
+
+library(ggbreak)
+p_global <- ggplot() +
+  geom_point(data = global, shape = 21, color = 'black',
+             aes(x = citation_short, y = n_lakes, 
+                 fill = rate_C_year, size = 1))  +
+  scale_y_break(c(1500, 92000),
+                scales = c(1, 7)) + # break between 1260 and 90000
+  scale_fill_gradient2(
+    low = "steelblue",       # for cooling
+    mid = "#D3D3D3",      # neutral
+    high = "firebrick",       # for warming
+    midpoint = 0) +
+  theme_bw() +
+  coord_cartesian(clip = "off", ylim = c(min(global$n_lakes) * 0.95, max(global$n_lakes) * 1.05)) +
+  ylab( 'Number of lakes') +
+  xlab('') +
+  ggtitle('Global studies') +
+  guides(size = 'none',
+         fill = 'none') +
+  labs(fill = 'Rate (°C/year)') +
+  theme(legend.position = "right",       # Keep legends on the right
+        legend.box = "horizontal",
+        #axis.text.x = element_text(angle = 45, hjust = 1),
+        text = element_text(size = 12),
+        plot.margin = unit(c(2, 1, 2, 1), "cm"),
+        axis.text.x = element_blank()) 
+
+p_global
+ggsave('./figures/landsat_7/global_studies_rates.png', p_global, 
+        dpi = 300, units = 'mm', height = 200, width = 250, scale = 0.5,
+       bg = 'transparent', type = 'cairo')
 
 summaries <- gps %>% 
   distinct(citation, .keep_all = TRUE)
@@ -181,53 +289,81 @@ summaries
 
 lakes <- summaries %>% 
   filter(n_lakes < 25000) %>% 
-  ggplot(aes(x = n_lakes)) +
-  geom_histogram(fill = 'black') +
+  ggplot(aes(x = n_lakes, fill = method_LSWT_standard)) +
+  geom_histogram() +
+  scale_fill_manual(values =  c(
+    "#E69F00",  # orange
+    "#009E73", # bluish green
+    "#0072B2",  # blue
+    "#D55E00", # reddish orange
+    "#56B4E9" # sky blue
+  )) +
   theme_bw() +
-  xlab('Number of lakes')
+  xlab('Study size (n lakes)') +
+  ylab('Frequency')+
+  labs(fill = 'Method of measurement')
 lakes
+
 years <- summaries %>% 
-  filter(n_lakes < 25000) %>% 
-  ggplot(aes(x = n_years)) +
-  geom_histogram(fill = 'black') +
+  ggplot(aes(x = n_years, fill = method_LSWT_standard)) +
+  geom_histogram() +
+  scale_fill_manual(values =  c(
+    "#E69F00",  # orange
+    "#009E73", # bluish green
+    "#0072B2",  # blue
+    "#D55E00", # reddish orange
+    "#56B4E9" # sky blue
+  )) +
   theme_bw() +
-  xlab('Number of years')
+  xlab('Study duration (n years)') +
+  ylab('Frequency')+
+  labs(fill = 'Method of measurement')
 years
+
 method <- summaries %>% 
-  filter(n_lakes < 25000,
-         method_LSWT_standard %in% c('in situ', 'modelled', 'satellite')) %>% 
   ggplot(aes(x = method_LSWT_standard, fill = spatial_extent)) +
   geom_bar() +
   theme_bw() +
-  scale_fill_viridis_d(option = "plasma") +  #theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
+  scale_fill_manual(values =  c(
+    "#E69F00",  # orange
+    "#009E73", # bluish green
+    "#0072B2",  # blue
+    "#D55E00", # reddish orange
+    "#56B4E9" # sky blue
+  )) +
   theme(legend.position = 'top',
         legend.key.size = unit(0.2, "cm")) +
   labs(fill = 'Spatial Extent') +
-  xlab('Method of measurement')+
-  ylab('Number of lakes') 
-
+  xlab('Study method (type)')+
+  ylab('Frequency') 
 method
 
 spatial <- summaries %>% 
-  filter(n_lakes < 25000) %>% 
-  ggplot(aes(x = spatial_extent)) +
+  ggplot(aes(x = spatial_extent, fill = method_LSWT_standard)) +
   geom_bar() +
+  scale_fill_manual(values =  c(
+    "#E69F00",  # orange
+    "#009E73", # bluish green
+    "#0072B2",  # blue
+    "#D55E00", # reddish orange
+    "#56B4E9" # sky blue
+  )) +
   theme_bw() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1)) +
-  xlab('Study spatial extent')
+  xlab('Study spatial extent') +
+  ylab('Frequency') +
+  labs(fill = 'Method of measurement')
+spatial
 
-max(gps$n_lakes)
-median(gps$n_lakes)
-median(gps$n_years)
-min(gps$n_years)
-max(gps$n_years)
-
-
-histos <- ggarrange(lakes, years, method, nrow = 1)
-ggarrange(mapc, histos, nrow = 2)
-
-
-# geom_bar()# make plot with the actual years on the x-axis geom_segment
+histos <- ggarrange(lakes, years, spatial, 
+                    nrow = 1,
+                    common.legend = TRUE, legend= 'bottom',
+                    labels = c('b', 'c', 'd'))
+histos
+description_plot <- ggarrange(p_notglobal, histos, nrow = 2,
+                              labels = c('a', '', '', ''))
+description_plot
+ggsave('./figures/landsat_7/map_histograms_lit_review.png', description_plot, 
+       dpi = 300, units = 'mm', height = 400, width = 650, scale = 0.3)
 
 gps <- gps %>% 
   group_by(temporal_aggregation) %>% 
@@ -241,7 +377,7 @@ gps$temporal_aggregation <- factor(gps$temporal_aggregation,
                                               'pre-rainy ', 'rainy ',
                                               'post-rainy'))
 
-gps %>% 
+time <- gps %>% 
   filter(n_lakes < 25000) %>% 
   ggplot() + 
   geom_segment(aes(x = min_year, xend = max_year, y = rate_C_year, yend = rate_C_year, 
@@ -252,6 +388,9 @@ gps %>%
   xlab('Duration of study') +
   ylab('Trend in LSWT (°C/year)') +
   labs(color = 'Number of lakes')
+time
+ggsave('./figures/landsat_7/timeframe_duration_lit_review.png', time, 
+       dpi = 300, units = 'mm', height = 400, width = 650, scale = 0.5)
 
 gps %>% 
   filter(n_lakes < 25000) %>% 
