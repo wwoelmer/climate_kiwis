56af772ef08660828f235450d12d83ad4b30849b
wwoelmer <wwoelmer@vt.edu>
2025-08-28 16:20:47 +1200

    move code from mapping script for descriptive stats

--- scripts/landsat_7/10_describe_lit_review.R
+++ scripts/landsat_7/10_describe_lit_review.R
@@ -1,3 +1,71 @@
+# calculate some summary/descriptive stats from lit review
+library(tidyverse)
+library(aemetools)
+library(ggplot2)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(RColorBrewer)
+library(scales)
+library(MetBrewer)
+library(viridis)
+library(ggpubr)
+
+gps <- read.csv('./data/LSWT_rates_of_change_literature.csv',
+                fileEncoding = 'latin1')
+
+gps$rate_C_year <- as.numeric(gps$rate_C_year)
+
+# get rid of this study on the map
+gps <- gps %>% 
+  filter(citation_short!='This study')
+
+mean(gps$rate_C_year, na.rm = TRUE)
+sum(gps$n_lakes, na.rm = TRUE)
+
+# change the spatial extent so it's either point, region, or global
+gps <- gps %>% 
+  mutate(spatial_extent = recode(spatial_extent,
+                                 'country' = 'region'))
+
+gps$spatial_extent <- factor(gps$spatial_extent, levels = c('point', 'region', 'global'))
+gps <- gps %>% 
+  filter(!is.na(rate_C_year))
+
+unique_study <- gps %>% 
+  distinct(citation, .keep_all = TRUE)
+
+table(unique_study$spatial_extent)
+table(gps$location)
+table(gps$method_LSWT)
+table(gps$method_LSWT_standard)
+table(gps$method_trend)
+table(gps$aggregation_mean_min_max_etc)
+table(gps$temporal_aggregation)
+max(gps$n_lakes)
+median(gps$n_lakes)
+median(gps$n_years)
+min(gps$n_years)
+max(gps$n_years)
+min(gps$rate_C_year)
+max(gps$rate_C_year)
+
+## night time estimates
+night <- gps %>% 
+  filter(str_detect(annual_seasonal_data, "night")) %>% 
+  distinct(citation)
+nrow(night)
+
+
+
+# filter to lakes that we can directly compare
+sub <- gps %>% 
+  filter(annual_seasonal_data=='annual',
+         aggregation_mean_min_max_etc=='mean')
+
+
+########################################
+## below is for formatting all studies cites, still a working example, not real data
 library(stringi)
 
 lit <- read.csv('./lit_review_example.csv')
